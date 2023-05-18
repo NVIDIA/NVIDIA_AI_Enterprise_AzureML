dbaa1245e44c41b479a06ab090e5535906e54054
jjdd88 <jjdd88@hotmail.com>
2023-05-18 16:08:26 -0700

    fix download break

--- src/components/DeepStream/download_models.sh
+++ src/components/DeepStream/download_models.sh
@@ -186,21 +186,23 @@ echo "==================================================================="
 echo "begin download models for dashcamnet / vehiclemakenet / vehicletypenet"
 echo " / trafficcamnet "
 echo "==================================================================="
-mkdir -p models/tao_pretrained_models/dashcamnet && \
+cd -
+mkdir -p models/tao_pretrained_models 
+mkdir -p models/tao_pretrained_models/dashcamnet 
 wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/tao/dashcamnet/versions/pruned_v1.0.2/zip \
 -O dashcamnet_pruned_v1.0.2.zip && unzip dashcamnet_pruned_v1.0.2.zip -d models/tao_pretrained_models/dashcamnet \
 && rm dashcamnet_pruned_v1.0.2.zip
-mkdir -p models/tao_pretrained_models/vehiclemakenet && \
+mkdir -p models/tao_pretrained_models/vehiclemakenet 
 wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/tao/vehiclemakenet/versions/pruned_v1.0.1/zip \
 -O vehiclemakenet_pruned_v1.0.1.zip && \
 unzip vehiclemakenet_pruned_v1.0.1.zip -d models/tao_pretrained_models/vehiclemakenet && \
 rm vehiclemakenet_pruned_v1.0.1.zip
-mkdir -p models/tao_pretrained_models/vehicletypenet && \
+mkdir -p models/tao_pretrained_models/vehicletypenet 
 wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/tao/vehicletypenet/versions/pruned_v1.0.1/zip \
 -O vehicletypenet_pruned_v1.0.1.zip && \
 unzip vehicletypenet_pruned_v1.0.1.zip -d models/tao_pretrained_models/vehicletypenet && \
 rm vehicletypenet_pruned_v1.0.1.zip
-mkdir -p models/tao_pretrained_models/trafficcamnet && \
+mkdir -p models/tao_pretrained_models/trafficcamnet 
 wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/tao/trafficcamnet/versions/pruned_v1.0.2/zip \
 -O trafficcamnet_pruned_v1.0.2.zip && \
 unzip trafficcamnet_pruned_v1.0.2.zip -d models/tao_pretrained_models/trafficcamnet && \
