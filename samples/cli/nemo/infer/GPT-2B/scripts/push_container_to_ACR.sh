d0daaf5470406167dbaf6c04ff1667676261be70
mreyesgomez <mreyesgomez@nvidia.com>
2023-09-21 13:26:35 -0700

    Adding initial scripts for NeMo Model Deployment with an AzureML Endpoint

--- samples/cli/nemo/infer/GPT-2B/scripts/push_container_to_ACR.sh
+++ samples/cli/nemo/infer/GPT-2B/scripts/push_container_to_ACR.sh
@@ -7,9 +7,9 @@ echo "az acr login -n $registryname"
 az acr login -n $registryname
 
 echo "Building the new docker image and tag it"
-docker build -t $registryname.azurecr.io/tao -f scripts/auxiliary_files/Dockerfile_aml.yml .
+docker build -t $registryname.azurecr.io/nemo_infer_gpt_2b -f scripts/auxiliary_files/Dockerfile_aml.yml .
 
 echo "Pushing the image to ACR"
-docker push ${registryname}.azurecr.io/tao:latest
+docker push ${registryname}.azurecr.io/nemo_infer_gpt_2b:latest
 
 
