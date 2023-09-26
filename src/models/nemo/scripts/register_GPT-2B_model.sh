5f4b43ae94b58943c298a46de317076053e4a782
mreyesgomez <mreyesgomez@nvidia.com>
2023-09-26 09:27:14 -0700

    Editis to model registering

--- src/models/nemo/scripts/register_GPT-2B_model.sh
+++ src/models/nemo/scripts/register_GPT-2B_model.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-source ../../../config_files/config.sh
 
-echo registering model GPT-2B to registry: $registry_name
 
 az ml model create --name GPT-2B_nemo --path ./scripts/models/nemo/GPT-2B --type custom_model --version 1 
