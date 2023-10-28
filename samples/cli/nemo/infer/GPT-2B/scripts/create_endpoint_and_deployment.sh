d028bb4371190a605b8176243f27f884d1b0dd85
Ubuntu <azureuser@nemo-a100-tester.0wuc3zjfqu3udnjhfkong2skwg.jx.internal.cloudapp.net>
2023-10-28 22:58:46 +0000

    NCD files

--- samples/cli/nemo/infer/GPT-2B/scripts/create_endpoint_and_deployment.sh
+++ samples/cli/nemo/infer/GPT-2B/scripts/create_endpoint_and_deployment.sh
@@ -6,9 +6,11 @@ source scripts/config_files/deployment_config.sh
 echo "Creating Online Endpoint"
 az ml online-endpoint create -f scripts/auxiliary_files/endpoint_aml.yml --resource-group $resource_group --workspace-name $workspace
 
-cp scripts/auxiliary_files/deployment_aml.yml scripts/auxiliary_files/actual_deployment_aml.yml
+cp scripts/auxiliary_files/deployment_backup2_aml.yml scripts/auxiliary_files/actual_deployment_aml.yml
 
 sed -i "s/registryname/${registryname}/g" scripts/auxiliary_files/actual_deployment_aml.yml
+
+cat scripts/auxiliary_files/actual_deployment_aml.yml
 
 echo "Creating Online Deployment"
 az ml online-deployment create -f scripts/auxiliary_files/actual_deployment_aml.yml
--- samples/cli/nemo/infer/GPT-2B/scripts/push_container_to_ACR.sh
+++ samples/cli/nemo/infer/GPT-2B/scripts/push_container_to_ACR.sh
@@ -7,7 +7,7 @@ echo "az acr login -n $registryname"
 az acr login -n $registryname
 
 echo "Building the new docker image and tag it"
-docker build -t $registryname.azurecr.io/nemo_infer_gpt_2b -f scripts/auxiliary_files/Dockerfile_aml.yml .
+docker build -t $registryname.azurecr.io/nemo_infer_gpt_2b -f scripts/auxiliary_files/Dockerfile2_aml.yml .
 
 echo "Pushing the image to ACR"
 docker push ${registryname}.azurecr.io/nemo_infer_gpt_2b:latest
